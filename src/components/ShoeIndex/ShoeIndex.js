463aa0de9749f69cdf81d6a4983bcd6453f88e70
Alan Wang <wzh19921016@gmail.com>
2024-12-15 15:42:10 -0800

    exercise 3 baseline

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -45,11 +45,13 @@ const ShoeIndex = ({ sortId, setSortId }) => {
 const Wrapper = styled.div`
   display: flex;
   flex-direction: row-reverse;
-  justify-content: space-between;
+  align-items: baseline;
   gap: 32px;
 `;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+  flex-basis: 248px;
+`;
 
 const MainColumn = styled.div`
   flex: 1;
@@ -58,6 +60,7 @@ const MainColumn = styled.div`
 const Header = styled.header`
   display: flex;
   justify-content: space-between;
+  align-items: baseline;
 `;
 
 const Title = styled.h2`
